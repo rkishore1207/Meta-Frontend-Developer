174765ac0ded10ff96d3e29f098bcc6d8e4033cb
Kishore <kishore1207india@gmail.com>
2023-10-26 10:29:54 +0530

    Execute some of built-in functions

---
+++ Programming with JavaScript/Week2/Week2-Practice.js
@@ -0,0 +1,11 @@
+var randomNumber = Math.random();
+console.log(randomNumber);
+var rounded = Math.ceil(randomNumber*10);
+console.log(rounded);
+
+//Some of the built-in functions
+console.log("ho-ho-ho".indexOf('h'));
+console.log("ho-ho-ho".lastIndexOf('h'));
+var name = "Kishore";
+console.log(name.charAt(0));
+console.log("ho-ho-ho".split('-'));
